350d9c5e600aef12d3fbd7829a11ac39e725a6e9
sasidharan <sasidharankpari@gmail.com>
2020-12-30 15:57:26 +0530

    added controllers and routes to deal with products

--- src/controllers/product.js
+++ src/controllers/product.js
@@ -1,43 +1,143 @@
 import express from 'express';
-import mongoose from 'mongoose';
+// import mongoose from 'mongoose';
 
 import productModel from '../models/productSchema.js';
 
 const router = express.Router();
 
-export const getAllProducts = async (req, res) => {
-	try {
-		const result = await productModel.find();
+export const getAllProducts = (req, res) => {
+	const limit = Number(req.query.limit) || 0;
+	const sort = (req.query.sort == 'desc') ? -1 : (req.query.sort == 'asc') ? 1 : 1 ;
 
-		res.status(200).json(result);
-	} catch (error) {
-		res.status(404).json({ message: error.message });
-	}
+	productModel.find()
+		.select(['-_id','-__v'])
+		.limit(limit)
+		.sort({ price: sort })
+		.then((result) => {
+			res.status(200).json(result);
+		})
+		.catch((error) => res.status(404).json({ message: error.message }));
 };
 
-export const getProductById = async (req, res) => {
-	const { id } = req.params;
+export const getProductById = (req, res) => {
+	const id = req.params;
+	productModel.findOne(id)
+		.select(['-_id','-__v'])
+		.then((result) => {
+			if (!result) {
+				res.status(404).json({
+					status: 'error',
+					message: 'productModel Not Found',
+					productId: `${id}`
+				});
+			}else{
+				res.status(200).json(result);
+			}
+		})
+		.catch((error) => res.status(404).json({ message: error.message }));
+};
 
-	if (!mongoose.Types.ObjectId.isValid(id)) {
-		return res.status(404).json({ message: `Invalid product id: ${id}` });
+export const getAllCategories = (req,res) => {
+
+	productModel.distinct('category')
+		.then((result) => {
+			if(!result.length){
+				res.status(404).json({ message: 'No Categories Found' });
+			}
+			res.status(200).json(result);
+		})
+		.catch((error) => res.status(404).json({ message: error.message }));
+};
+
+
+export const getProductsByCategory = (req,res) => {
+	const category = req.params.category;
+	const limit = Number(req.query.limit) || 0;
+
+	productModel.distinct('category')
+		.find({
+			category,
+		})
+		.limit(limit)
+		.then((result) => {
+			if(!result.length){
+				res.status(404).json({ message: 'No Such Category' });
+			}else{res.status(200).json(result);
+			}
+			
+		})
+		.catch((error) => res.status(404).json({ message: error.message }));
+
+};
+export const addProduct = (req, res) => {
+	if (typeof req.body == undefined) {
+		res.json({
+			status: 'error',
+			message: 'data is undefined',
+		});
+	} else {
+		let productCount = 0;
+		productModel.find()
+			.countDocuments(function (err, count) {
+				productCount = count;
+			})
+			.then(() => {
+				const result = new productModel({
+					id: productCount + 1,
+					name: req.body.name,
+					price: req.body.price,
+					description: req.body.description,
+					image: req.body.image,
+					category: req.body.category,
+				});
+				result.save()
+					.then(result => res.json(result))
+					.catch(error => console.log(error));
+			});
 	}
-	try {
-		const result = await productModel.findById(id);
+};
+  
+export const editProduct = (req, res) => {
+	const id = req.params.id;
+	if (typeof req.body == undefined || id == null) {
+		res.json({
+			status: 'error',
+			message: 'Re check your product data',
+		});
+	} 
+	const productInfo= {
+		name: req.body.name,
+		price: req.body.price,
+		description: req.body.description,
+		image: req.body.image,
+		category: req.body.category,
+	};
+	productModel.findOneAndUpdate({id:id}, productInfo, {new: true})
+		.select(['-_id','-__v'])
+		.then((result) => {
+			res.json(result);
+		})
+		.catch((error) => res.status(500).json({ message: error.message }));
 
-		if (!result) {
-			res.
-				status(404).
-				json(
-					{
-						error: { message: 'Product Not Found' },
-						productId: `${id}`
-					}
-
-				);
-		}
-		res.status(200).json(result);
-	} catch (error) {
-		res.status(500).json({ message: error.message });
+};
+  
+export const deleteProduct = (req, res) => {
+	if (req.params.id == null) {
+		res.json({
+			status: 'error',
+			message: 'product id cant be empty',
+		});
+	} else {
+		productModel.findOneAndDelete({id: req.params.id})
+			.select(['-_id','-__v'])
+			.then((result) => {
+				if(!result){
+					res.status(404).json({ message: 'Product Not Found' });
+				}else{
+					res.status(200).json(result);
+				}
+			})
+			.catch((error) => res.status(404).json({ message: error.message }));
 	}
 };
 
--- src/routes/product.js
+++ src/routes/product.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { getAllProducts, getProductById } from '../controllers/product.js';
+import { getAllProducts, getAllCategories, getProductsByCategory, getProductById, addProduct, editProduct, deleteProduct} from '../controllers/product.js';
 
 const router = express.Router();
 
 router.get('/', getAllProducts);
+router.get('/categories', getAllCategories);
+router.get('/category/:category', getProductsByCategory);
 router.get('/:id', getProductById);
+router.post('/', addProduct);
+router.put('/:id', editProduct);
+router.delete('/:id', deleteProduct);
 
 export default router;
