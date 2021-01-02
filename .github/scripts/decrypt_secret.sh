d248c0ca405a5106906c858a28f24d25f4e130a0
sasidharan <sasidharankpari@gmail.com>
2021-01-03 01:46:13 +0530

    Update decrypt_secret.sh

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -1,4 +1,11 @@
 # Decrypt the file
+SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+echo $SCRIPTPATH
+
+for entry in "$SCRIPTPATH"/*
+do
+  echo "$entry"
+done
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output .env.dev .env.dev.gpg
 gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output .env.test .env.test.gpg
 
