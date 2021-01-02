37fe81d875a07072846fabc272b148db1c78acfa
sasidharankp <sasidharan1993@gmail.com>
2021-01-03 00:03:22 +0530

    Update decrypt_secret.sh

--- .github/workflows/decrypt_secret.sh
+++ .github/workflows/decrypt_secret.sh
@@ -6,5 +6,6 @@ for entry in "$SCRIPTPATH"/*
 do
   echo "$entry"
 done
+
 # gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output $HOME/.env.dev ../../.env.dev.gpg
 # gpg --quiet --batch --yes --decrypt --passphrase="$SECRET_PASSPHRASE" --output $HOME/.env.test ../../.env.test.gpg
