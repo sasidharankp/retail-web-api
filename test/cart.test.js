f3d85be7ae71723f60e3338527231545da8a831e
sasidharan <sasidharankpari@gmail.com>
2021-01-07 00:47:18 +0530

    Update cart.test.js

--- test/cart.test.js
+++ test/cart.test.js
@@ -30,7 +30,7 @@ describe('/carts Routes', () => {
 			.post('/products')
 			.send(productInfo)
 			.end((err, res) => {
-				console.log(res.body)
+				console.log(res.body);
 				expect(res.status).to.deep.eql(200);
 				expect(res.body).to.be.an('object')
 					.that.have.all.keys([ 'productId', 'category', 'name', 'price', 'stock', 'description', 'image', '_id', '__v' ]);
